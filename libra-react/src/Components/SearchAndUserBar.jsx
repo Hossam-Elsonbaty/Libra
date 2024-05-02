a685cf9e0ca2e4030c32edf4d4b4ed4153e4c6f7
ahmadhossam24 <70440432+ahmadhossam24@users.noreply.github.com>
2024-05-02 17:52:30 +0300

    adminHover

--- libra-react/src/Components/SearchAndUserBar.jsx
+++ libra-react/src/Components/SearchAndUserBar.jsx
@@ -13,6 +13,17 @@ export default function SearchAndUserBar() {
           <div><i class="fa fa-comment"></i><div className="comment-notification"></div></div>
           <div className="userImg">
             <img src="https://th.bing.com/th/id/OIP.4G05enziAqYAOfK58iNxQgHaHa?w=212&h=212&c=7&r=0&o=5&dpr=1.3&pid=1.7" alt=""/>
+            <div  className="admin-settings-div">
+              <div className="admin-welcome-div">
+                <p className="admin-welcome-div-first-p">Welcome Admin !</p>
+                <p className="admin-welcome-div-second-p">Travor james</p>
+              </div>
+              <div className="admin-buttons-div">
+                <span><p>My Profile</p><i className="fa fa-user"></i></span>
+                <span><p>Settings</p><i className="fa fa-cog"></i></span>
+                <span><p>Logout</p><i className="fa fa-sign-out"></i></span>
+              </div>
+            </div>
           </div>
         </div>
       </div>
